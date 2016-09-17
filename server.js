8a429121fef8c1495ab00d0bb019b409942c5734
Andrew MacMurray <a.macmurray@icloud.com>
2016-09-17 12:32:36 +0100

    separate server into index and server (easier to test if we want to), refactor server js

--- server.js
+++ server.js
@@ -1,39 +1,28 @@
 var Hapi = require('hapi');
-var Inert = require('inert');
-var Vision = require('vision');
-var Handlebars = require('handlebars');
+var server = new Hapi.Server();
+var path = require('path');
 
 var routes = require('./lib/routes.js');
+var plugins = [
+  'inert',
+  'vision'
+].map(require);
 
-var server = new Hapi.Server();
-
-server.register(Inert, function () {
-    server.connection({
-        port: process.env.PORT || 8080
-    });
-
-    server.route(routes);
-
-});
-
-server.register(Vision, function() {
-    server.views({
-        engines: {
-            html: Handlebars
-        },
-        relativeTo: __dirname + '/public/views',
-        path: '.',
-        layout: 'default',
-        layoutPath: 'layout',
-        helpersPath: 'helpers',
-        partialsPath: 'partials'
-    });
-
-});
-
-server.start(function() {
-    console.log('Server running at: ' + server.info.uri + '!');
-
+server.connection({ port: process.env.PORT || 8080 });
+server.register(plugins, function (err) {
+  if (err) throw err;
+  server.route(routes);
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: path.join(__dirname, '/public/views'),
+    path: '.',
+    layout: 'default',
+    layoutPath: 'layout',
+    helpersPath: 'helpers',
+    partialsPath: 'partials'
+  });
 });
 
 module.exports = server;
